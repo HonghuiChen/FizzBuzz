6cacb80e88323c98003b9e0e482fac632987283c
HonghuiChen <willhh.chen@mail.utoronto.ca>
2023-09-13 18:37:06 -0400

    reduce

---
+++ src/Reduce.java
@@ -0,0 +1,23 @@
+public class Reduce {
+
+    public static void main(String[] args) {
+
+        int steps = 0;
+        int n = 100;
+
+        while (n != 0) {
+            boolean even = n % 2 == 0;
+            boolean odd = n % 2 == 1;
+
+            if (even) {
+                n = n / 2;
+            }
+            else if (odd) {
+                n = n - 1;
+            }
+            steps++;
+        }
+
+        System.out.println(steps);
+    }
+}
